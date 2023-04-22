eba16558e1fead41edacd85b38c701e1d7d67491
Gegardus <stgegard@gmail.com>
2023-04-22 23:40:58 +0400

    add navbar and card components

---
+++ src/components/Card.js
@@ -0,0 +1,15 @@
+function Card({index}) {
+  return (
+    <div key={index} className="col mb-5">
+      <div className="card" style={{ width: "18rem" }}>
+        <img
+          src="https://via.placeholder.com/200"
+          className="card-img-top"
+          alt="..."
+        />
+      </div>
+    </div>
+  );
+}
+
+export default Card;
